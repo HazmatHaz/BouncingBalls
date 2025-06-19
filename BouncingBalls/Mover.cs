3322ecb5cd31ce4ba4a77b688266f74339954835
Harrison Ellis <231635@live.brock.ac.uk>
2025-06-19 13:51:17 +0100

    Add project files.

---
+++ BouncingBalls/Mover.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Drawing;
+using System.Numerics;
+
+namespace BouncingBalls
+{
+    class Mover
+    {
+        static Random rnd = new Random();
+        Vector2 location;
+        Vector2 Velocity;
+
+        //constructor method
+        public Mover(int x, int y)
+        {
+            location = new Vector2(rnd.Next(x),rnd.Next(y));
+            Velocity = new Vector2(rnd.Next(-10, 11), rnd.Next(-10,11));
+        }
+
+        public void Update()
+        {
+            Velocity.X = rnd.Next(-10, 11);
+            Velocity.Y = rnd.Next(-10, 11);
+            location = Vector2.Add(Velocity, location);
+        }
+        public void Display(Graphics e)
+        {
+            e.FillEllipse(Brushes.Aquamarine, location.X, location.Y, 100, 100);
+        }
+    }
+
+}
